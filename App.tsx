fa9f43e79f48cfa5c05d1cb8416608bb92633195
Gnauqoa <quanglng2022@gmail.com>
2023-11-13 00:27:35 +0700

    complete logic

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import {
   StyleSheet,
@@ -13,12 +13,13 @@ const Row = ({ children }: { children: any }) => (
   <View style={styles.row}>{children}</View>
 );
 
-// ADDED
 interface IButton {
   value: string;
   style?: "secondary" | "accent" | "double";
+  onPress: () => void;
 }
-const Button = ({ value, style }: IButton) => {
+
+const Button = ({ value, style, onPress }: IButton) => {
   const btnStyles: any[] = [styles.btn];
   const txtStyles: any[] = [styles.btnText];
   if (style === "secondary") {
@@ -32,57 +33,147 @@ const Button = ({ value, style }: IButton) => {
     btnStyles.push(styles.btnDouble);
   }
   return (
-    <TouchableOpacity style={btnStyles} onPress={() => console.log(value)}>
+    <TouchableOpacity style={btnStyles} onPress={onPress}>
       <Text style={txtStyles}>{value}</Text>
     </TouchableOpacity>
   );
 };
 
-// BUTTONS ADDED
 export default function App() {
-  const computedValue = 123456.23;
+  const [displayValue, setDisplayValue] = useState("0");
+  const [operator, setOperator] = useState("");
+  const [storedValue, setStoredValue] = useState("");
+
+  const handleNumberPress = (value: string) => {
+    if (displayValue === "0") {
+      setDisplayValue(value);
+    } else {
+      setDisplayValue(displayValue + value);
+    }
+    if (parseFloat(displayValue) < 0) {
+      setDisplayValue("-" + displayValue);
+    }
+  };
+
+  const handleDecimalPress = () => {
+    if (!displayValue.includes(".")) {
+      setDisplayValue(displayValue + ".");
+    }
+  };
+
+  const handleOperatorPress = (value: string) => {
+    if (value === "%") {
+      const currentValue = parseFloat(displayValue);
+      const result = currentValue / 100;
+      setDisplayValue(result.toString());
+    } else if (value === "+/-") {
+      setDisplayValue((parseFloat(displayValue) * -1).toString());
+    } else {
+      setOperator(value);
+      setStoredValue(displayValue);
+      setDisplayValue("0");
+    }
+  };
+
+  const handleEqualsPress = () => {
+    const currentValue = parseFloat(displayValue);
+    const storedValueFloat = parseFloat(storedValue);
+    let result = 0;
+    switch (operator) {
+      case "+":
+        result = storedValueFloat + currentValue;
+        break;
+      case "-":
+        result = storedValueFloat - currentValue;
+        break;
+      case "x":
+        result = storedValueFloat * currentValue;
+        break;
+      case "/":
+        result = storedValueFloat / currentValue;
+        break;
+      default:
+        break;
+    }
+    setDisplayValue(result.toString());
+    setOperator("");
+    setStoredValue("");
+  };
+
+  const handleClearPress = () => {
+    setDisplayValue("0");
+    setOperator("");
+    setStoredValue("");
+  };
 
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
       <SafeAreaView>
-        <Text style={styles.computedValue}>
-          {computedValue.toLocaleString()}
-        </Text>
+        <Text style={styles.computedValue}>{displayValue}</Text>
 
         <Row>
-          <Button value="C" style="secondary" />
-          <Button value="+/-" style="secondary" />
-          <Button value="%" style="secondary" />
-          <Button value="/" style="accent" />
+          <Button value="C" style="secondary" onPress={handleClearPress} />
+          <Button
+            value="+/-"
+            style="secondary"
+            onPress={() => handleOperatorPress("+/-")}
+          />
+          <Button
+            value="%"
+            style="secondary"
+            onPress={() => handleOperatorPress("%")}
+          />
+          <Button
+            value="/"
+            style="accent"
+            onPress={() => handleOperatorPress("/")}
+          />
         </Row>
         <Row>
-          <Button value="7" />
-          <Button value="8" />
-          <Button value="9" />
-          <Button value="x" style="accent" />
+          <Button value="7" onPress={() => handleNumberPress("7")} />
+          <Button value="8" onPress={() => handleNumberPress("8")} />
+          <Button value="9" onPress={() => handleNumberPress("9")} />
+          <Button
+            value="x"
+            style="accent"
+            onPress={() => handleOperatorPress("x")}
+          />
         </Row>
         <Row>
-          <Button value="4" />
-          <Button value="5" />
-          <Button value="6" />
-          <Button value="-" style="accent" />
+          <Button value="4" onPress={() => handleNumberPress("4")} />
+          <Button value="5" onPress={() => handleNumberPress("5")} />
+          <Button value="6" onPress={() => handleNumberPress("6")} />
+          <Button
+            value="-"
+            style="accent"
+            onPress={() => handleOperatorPress("-")}
+          />
         </Row>
         <Row>
-          <Button value="1" />
-          <Button value="2" />
-          <Button value="3" />
-          <Button value="+" style="accent" />
+          <Button value="1" onPress={() => handleNumberPress("1")} />
+          <Button value="2" onPress={() => handleNumberPress("2")} />
+          <Button value="3" onPress={() => handleNumberPress("3")} />
+          <Button
+            value="+"
+            style="accent"
+            onPress={() => handleOperatorPress("+")}
+          />
         </Row>
         <Row>
-        <Button value="0" style="double" />
-          <Button value="." />
-          <Button value="=" style="accent" />
+          <Button
+            value="0"
+            style="double"
+            onPress={() => handleNumberPress("0")}
+          />
+          <Button value="." onPress={handleDecimalPress} />
+          <Button value="=" style="accent" onPress={handleEqualsPress} />
         </Row>
       </SafeAreaView>
     </View>
   );
 }
+
 const BTN_MARGIN = 5;
 const screen = Dimensions.get("window");
 const buttonWidth = screen.width / 4 - BTN_MARGIN * 2;
