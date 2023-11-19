cedf01d7205a7814ecd9dba617b29ded4788ec25
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2023-11-19 17:59:20 +0700

    wip

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import * as ScreenOrientation from "expo-screen-orientation";
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import {
@@ -8,6 +9,7 @@ import {
   TouchableOpacity,
   Dimensions,
   Clipboard,
+  useWindowDimensions,
 } from "react-native";
 
 const Row = ({ children }: { children: any }) => (
@@ -18,10 +20,23 @@ interface IButton {
   value: string;
   style?: "secondary" | "accent" | "double";
   onPress: () => void;
+  mode: number;
 }
 
-const Button = ({ value, style, onPress }: IButton) => {
-  const btnStyles: any[] = [styles.btn];
+const Button = ({ value, style, onPress, mode }: IButton) => {
+  const { height, width } = useWindowDimensions();
+  const isLandScape = mode !== ScreenOrientation.Orientation.PORTRAIT_UP;
+  const buttonWidth = isLandScape
+    ? width / 4 - BTN_MARGIN * 2
+    : width / 4 - BTN_MARGIN * 2;
+  const buttonHeight = isLandScape
+    ? height / 9 - BTN_MARGIN * 2
+    : height / 8 - BTN_MARGIN * 2;
+
+  const btnStyles: any[] = [
+    styles.btn,
+    { width: buttonWidth, height: buttonHeight },
+  ];
   const txtStyles: any[] = [styles.btnText];
   if (style === "secondary") {
     btnStyles.push(styles.btnSecondary);
@@ -30,11 +45,15 @@ const Button = ({ value, style, onPress }: IButton) => {
   if (style === "accent") {
     btnStyles.push(styles.btnAccent);
   }
-  if (style === "double") {
-    btnStyles.push(styles.btnDouble);
-  }
+  // if (style === "double") {
+  //   btnStyles.push(styles.btnDouble);
+  //   btnStyles.push({
+  //     width: buttonWidth,
+  //     height: buttonHeight,
+  //   });
+  // }
   return (
-    <TouchableOpacity style={btnStyles} onPress={onPress}>
+    <TouchableOpacity style={[btnStyles]} onPress={onPress}>
       <Text style={txtStyles}>{value}</Text>
     </TouchableOpacity>
   );
@@ -44,6 +63,7 @@ export default function App() {
   const [displayValue, setDisplayValue] = useState("0");
   const [operator, setOperator] = useState("");
   const [storedValue, setStoredValue] = useState("");
+  const [mode, setMode] = useState(1);
 
   const handleNumberPress = (value: string) => {
     if (displayValue === "0") {
@@ -116,55 +136,188 @@ export default function App() {
     setDisplayValue(clipboardContent);
   };
 
+  const handleLandscapeMode = async () => {
+    const mode = await ScreenOrientation.getOrientationAsync();
+    if (mode === ScreenOrientation.Orientation.PORTRAIT_UP) {
+      ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+
+      setMode(0);
+    } else {
+      ScreenOrientation.lockAsync(
+        ScreenOrientation.OrientationLock.PORTRAIT_UP
+      );
+      setMode(1);
+    }
+  };
+
+  const handleTrigPress = (trigFunction: string) => {
+    const currentValue = parseFloat(displayValue);
+    let result = 0;
+    switch (trigFunction) {
+      case "sin":
+        result = Math.sin(currentValue);
+        break;
+      case "cos":
+        result = Math.cos(currentValue);
+        break;
+      case "tan":
+        result = Math.tan(currentValue);
+        break;
+      default:
+        break;
+    }
+    setDisplayValue(result.toString());
+  };
+
+  const handleLogPress = () => {
+    const currentValue = parseFloat(displayValue);
+    const result = Math.log10(currentValue);
+    setDisplayValue(result.toString());
+  };
+
+  const handleLnPress = () => {
+    const currentValue = parseFloat(displayValue);
+    const result = Math.log(currentValue);
+    setDisplayValue(result.toString());
+  };
+
+  const handleInversePress = () => {
+    const currentValue = parseFloat(displayValue);
+    const result = 1 / currentValue;
+    setDisplayValue(result.toString());
+  };
+
+  const isLandScape = mode !== ScreenOrientation.Orientation.PORTRAIT_UP;
+
   return (
-    <View style={styles.container}>
+    <View
+      style={[
+        styles.container,
+        mode !== ScreenOrientation.Orientation.PORTRAIT_UP
+          ? styles.containerLandscape
+          : null,
+      ]}
+    >
       <StatusBar style="light" />
-      <SafeAreaView>
+      <SafeAreaView style={{ width: "100%" }}>
         <Text style={styles.computedValue}>{displayValue}</Text>
 
         <Row>
-          <Button value="C" style="secondary" onPress={handleClearPress} />
           <Button
+            mode={mode}
+            value="C"
+            style="secondary"
+            onPress={handleClearPress}
+          />
+          <Button
+            mode={mode}
             value="+/-"
             style="secondary"
             onPress={() => handleOperatorPress("+/-")}
           />
           <Button
+            mode={mode}
             value="%"
             style="secondary"
             onPress={() => handleOperatorPress("%")}
           />
           <Button
+            mode={mode}
             value="/"
             style="accent"
             onPress={() => handleOperatorPress("/")}
           />
         </Row>
         <Row>
-          <Button value="7" onPress={() => handleNumberPress("7")} />
-          <Button value="8" onPress={() => handleNumberPress("8")} />
-          <Button value="9" onPress={() => handleNumberPress("9")} />
+          {isLandScape && (
+            <>
+              <Button
+                mode={mode}
+                value="sin"
+                onPress={() => handleTrigPress("sin")}
+              />
+              <Button
+                mode={mode}
+                value="cos"
+                onPress={() => handleTrigPress("cos")}
+              />
+              <Button
+                mode={mode}
+                value="tan"
+                onPress={() => handleTrigPress("tan")}
+              />
+            </>
+          )}
           <Button
+            mode={mode}
+            value="7"
+            onPress={() => handleNumberPress("7")}
+          />
+          <Button
+            mode={mode}
+            value="8"
+            onPress={() => handleNumberPress("8")}
+          />
+          <Button
+            mode={mode}
+            value="9"
+            onPress={() => handleNumberPress("9")}
+          />
+          <Button
+            mode={mode}
             value="x"
             style="accent"
             onPress={() => handleOperatorPress("x")}
           />
         </Row>
         <Row>
-          <Button value="4" onPress={() => handleNumberPress("4")} />
-          <Button value="5" onPress={() => handleNumberPress("5")} />
-          <Button value="6" onPress={() => handleNumberPress("6")} />
+          {isLandScape && (
+            <>
+              <Button mode={mode} value="1/x" onPress={handleInversePress} />
+              <Button mode={mode} value="log" onPress={handleLogPress} />
+              <Button mode={mode} value="ln" onPress={handleLnPress} />
+            </>
+          )}
           <Button
+            mode={mode}
+            value="4"
+            onPress={() => handleNumberPress("4")}
+          />
+          <Button
+            mode={mode}
+            value="5"
+            onPress={() => handleNumberPress("5")}
+          />
+          <Button
+            mode={mode}
+            value="6"
+            onPress={() => handleNumberPress("6")}
+          />
+          <Button
+            mode={mode}
             value="-"
             style="accent"
             onPress={() => handleOperatorPress("-")}
           />
         </Row>
         <Row>
-          <Button value="1" onPress={() => handleNumberPress("1")} />
-          <Button value="2" onPress={() => handleNumberPress("2")} />
-          <Button value="3" onPress={() => handleNumberPress("3")} />
           <Button
+            mode={mode}
+            value="1"
+            onPress={() => handleNumberPress("1")}
+          />
+          <Button
+            mode={mode}
+            value="2"
+            onPress={() => handleNumberPress("2")}
+          />
+          <Button
+            mode={mode}
+            value="3"
+            onPress={() => handleNumberPress("3")}
+          />
+          <Button
+            mode={mode}
             value="+"
             style="accent"
             onPress={() => handleOperatorPress("+")}
@@ -172,16 +325,33 @@ export default function App() {
         </Row>
         <Row>
           <Button
+            mode={mode}
             value="0"
             style="double"
             onPress={() => handleNumberPress("0")}
           />
-          <Button value="." onPress={handleDecimalPress} />
-          <Button value="=" style="accent" onPress={handleEqualsPress} />
+          <Button mode={mode} value="." onPress={handleDecimalPress} />
+          <Button
+            mode={mode}
+            value="="
+            style="accent"
+            onPress={handleEqualsPress}
+          />
         </Row>
         <Row>
-          <Button value="Paste" onPress={handlePastePress} />
-          <Button value="Copy Result" onPress={handleCopyPress} />
+          <Button mode={mode} value="Paste" onPress={handlePastePress} />
+          <Button mode={mode} value="Copy Result" onPress={handleCopyPress} />
+        </Row>
+        <Row>
+          <Button
+            mode={mode}
+            value={
+              mode === ScreenOrientation.Orientation.PORTRAIT_UP
+                ? "Portrait"
+                : "Landscape"
+            }
+            onPress={handleLandscapeMode}
+          />
         </Row>
       </SafeAreaView>
     </View>
@@ -189,14 +359,16 @@ export default function App() {
 }
 
 const BTN_MARGIN = 5;
-const screen = Dimensions.get("window");
-const buttonWidth = screen.width / 4 - BTN_MARGIN * 2;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#202020",
-    justifyContent: "flex-end",
+    // justifyContent: "flex-end",
+    flexDirection: "column",
+  },
+  containerLandscape: {
+    flexDirection: "row",
   },
 
   computedValue: {
@@ -223,7 +395,14 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     margin: BTN_MARGIN,
     borderRadius: 100,
-    height: buttonWidth,
+  },
+  btn2: {
+    backgroundColor: "#333333",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    margin: BTN_MARGIN,
+    borderRadius: 100,
   },
   btnSecondary: {
     backgroundColor: "#a6a6a6",
@@ -237,7 +416,5 @@ const styles = StyleSheet.create({
   btnDouble: {
     alignItems: "flex-start",
     flex: 0,
-    width: buttonWidth * 2 + BTN_MARGIN * 2,
-    paddingLeft: buttonWidth / 2 - BTN_MARGIN * 1.5,
   },
 });
